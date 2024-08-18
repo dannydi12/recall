fea682047b6169fce6887e4c789d018e4e5a8edc
Daniel Di Venere <danieldivenere@gmail.com>
2024-08-18 12:43:19 -0700

    tailwind setup

--- index.tsx
+++ index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./src/App";
 import "./src/index.css";
+import { ThemeProvider } from "@/system/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button } from "@/system/ui/button";
+import { ModeToggle } from "./system/mode-toggle";
+import { Card } from "./system/ui/card";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="p-4">
+      <Card className="p-4">
+        <ModeToggle />
+      </Card>
       <h1>Hello, Electron with React?</h1>
+      <Button>Hey</Button>
     </div>
   );
 };
