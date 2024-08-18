eb298928b03e3aef905898c7c6a05323c206e85a
Daniel Di Venere <danieldivenere@gmail.com>
2024-08-18 13:10:06 -0700

    conditional rendering

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import ROUTES from "@/utils/routes";
 
 import { cn } from "@/lib/utils";
 
 const Sidebar: React.FC = () => {
+  const { pathname } = useLocation();
   return (
     <nav className="grid gap-4 text-sm text-muted-foreground border-r pr-4">
-      <Link to={ROUTES.base} className="font-semibold text-primary">
+      <Link
+        to={ROUTES.base}
+        className={cn({
+          "font-semibold text-primary": pathname === ROUTES.base,
+        })}
+      >
         Dashboard
       </Link>
-      <Link to="#">Bookmarks</Link>
-      <Link to={ROUTES.settings}>Settings</Link>
+      <Link
+        className={cn({ "font-semibold text-primary": pathname === "#" })}
+        to="#"
+      >
+        Bookmarks
+      </Link>
+      <Link
+        className={cn({
+          "font-semibold text-primary": pathname === ROUTES.settings,
+        })}
+        to={ROUTES.settings}
+      >
+        Settings
+      </Link>
     </nav>
   );
 };
